4489d3e848348739b36518e7faae15164c646bc7
Kika <anamonteiro430@gmail.com>
2020-08-12 14:59:16 +0100

    basic structure of components

---
+++ src/Components/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Todo } from './Todo';
+import { Todos_Info } from './Todos_Info';
+
+export const Main = () => {
+  return <div className="main"></div>;
+};
---
+++ src/Components/Todos_Info.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Todos_Info = () => {
+  return <div className="main"></div>;
+};
